2541639365e4bad0ee6c3a6505d42b4f835ce22b
Adam King <secure@adamking.eu>
2015-04-16 00:59:58 +0100

    Refactoring stills event loop.

--- camera.py
+++ camera.py
@@ -14,48 +14,44 @@ import zmqoutput
 
 log = logging.getLogger(__name__)
 
-jpeg_requests = {}
 
-def video_loop(videosocket, jpegsocket, eventsocket, config):
-    """Read video stream from camera in blocks and publish 
-    on ZeroMQ socket for recording and watching on other
-    devices.
+def stills_event_loop(jpegsocket, camera, config):
+    """This function handles any incoming requests for JPEG stills.
+    Any clients requesting stills receive back chunks of the file
+    and must send continuation requests.
     """
-                camera.start_recording(video_output, format='h264', motion_output=motion_detector)
 
-                # The camera is up and running so now we poll JPEG request socket for
-                # any requests for a JPEG still which is delivered piece by piece.
-                poller = zmq.Poller()
-                poller.register(jpegsocket, zmq.POLLIN)
-                while True:
-                    socks = dict(poller.poll())
-                    if jpegsocket in socks and socks[jpegsocket] == zmq.POLLIN:
-                        address, empty, req = jpegsocket.recv_multipart()
-                        handle_jpeg_request(camera, jpegsocket, net_frame_size, address, req)
+    poller = zmq.Poller()
+    poller.register(jpegsocket, zmq.POLLIN)
 
+    jpeg_requests = {}
 
-def handle_jpeg_request(camera, jpegsocket, net_frame_size, address, req):
-    global jpeg_requests
+    while True:
+        socks = dict(poller.poll())
+        if jpegsocket in socks and socks[jpegsocket] == zmq.POLLIN:
+            address, empty, req = jpegsocket.recv_multipart()
 
-    if req == 'NEW':
-        if address in jpeg_requests:
-            jpeg_requests[address].close()
-            del jpeg_requests[address]
+            if req == 'NEW':
+                log.info('Request for JPEG still received from %s.', address)
+                if address in jpeg_requests:
+                    jpeg_requests[address].close()
+                    del jpeg_requests[address]
 
-        # Create new stream and capture image to memory.
-        jpeg_requests[address] = io.BytesIO()
-        camera.capture(jpeg_requests[address], use_video_port=True)
-        data = jpeg_requests[address].read(net_frame_size)
+                # Create new stream and capture image to memory.
+                jpeg_requests[address] = io.BytesIO()
+                camera.capture(jpeg_requests[address], use_video_port=True)
+                data = jpeg_requests[address].read(net_frame_size)
 
-    elif req == 'NEXT':
-        # Check for still in progress and send next frame.
-        data = jpeg_requests[address].read(net_frame_size)
+            elif req == 'NEXT':
+                # Check for still in progress and send next frame.
+                data = jpeg_requests[address].read(net_frame_size)
 
-    elif req == 'ENDACK':
-        # Transfer is complete and file acknowledged.
-        data = b''
+            elif req == 'ENDACK':
+                # Transfer is complete and file acknowledged.
+                log.info('JPEG still transfer to %s acknowledged complete.', address)
+                data = b''
         
-    jpegsocket.send_multipart([address, b'', data])
+            jpegsocket.send_multipart([address, b'', data])
 
 
 def main():
@@ -66,24 +62,6 @@ def main():
     log.info('Loading config from %s.', configfile)
     configdata = config.load_from_file(configfile)
     
-    eventport = configdata['network']['event_pub_port']
-    videoport = configdata['network']['h264_pub_port']
-    jpegport = configdata['network']['jpeg_router_port']
-    
-    context = zmq.Context()
-    log.info('Binding event socket to port %i.', eventport)
-    eventsocket = context.socket(zmq.PUB)
-    eventsocket.bind('tcp://*:%s' % eventport)
-    
-    log.info('Binding video socket to port %i.', videoport)
-    videosocket = context.socket(zmq.PUB)
-    videosocket.bind('tcp://*:%s' % videoport)
-
-    log.info('Binding jpeg socket to port %i.', jpegport)
-    jpegsocket = context.socket(zmq.ROUTER)
-    jpegsocket.bind('tcp://*:%s' % jpegport)
-
-    log.info('Assembling configuration variables.')
     magnitude_threshold = config['motion']['magnitude_threshold']
     block_threshold = config['motion']['block_threshold']
 
@@ -91,8 +69,25 @@ def main():
     framerate = config['camera']['framerate']
 
     net_frame_size = config['network']['net_frame_size']
+    eventport = configdata['network']['event_pub_port']
+    videoport = configdata['network']['h264_pub_port']
+    jpegport = configdata['network']['jpeg_router_port']
+    
+    context = zmq.Context()
+    log.info('Binding event publish socket to port %i.', eventport)
+    eventsocket = context.socket(zmq.PUB)
+    eventsocket.bind('tcp://*:%s' % eventport)
+    
+    log.info('Binding video publish socket to port %i.', videoport)
+    videosocket = context.socket(zmq.PUB)
+    videosocket.bind('tcp://*:%s' % videoport)
+
+    log.info('Binding JPEG router socket to port %i.', jpegport)
+    jpegsocket = context.socket(zmq.ROUTER)
+    jpegsocket.bind('tcp://*:%s' % jpegport)
 
     with picamera.PiCamera() as camera:
+
         with zmqoutput.ZeroMqOutput(videosocket, net_frame_size) as video_output:
     
             with motiondetection.VectorThresholdMotionDetect(motion_event_handler, 
@@ -100,10 +95,10 @@ def main():
                                                              block_threshold,
                                                              camera) as motion_detector:
         
-
-    
-                log.info('Entering video processing loop.')
-                video_loop(camera, video_output, motion_detector)
+                log.info('Starting camera video capture.')
+                camera.start_recording(video_output, format='h264', motion_output=motion_detector)
+                log.info('Entering JPEG still event loop.')
+                stills_event_loop(camera, video_output, motion_detector)
 
 
 if __name__ == '__main__':
