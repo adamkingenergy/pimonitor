236b66cf31286b4160599963cf80365112061b7c
Adam King <git@adamking.eu>
2015-04-20 23:16:53 +0100

    Adding monitor process.

---
+++ gui/imagepanel.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import wx
+import logging
+
+log = logging.getLogger(__name__)
+ 
+class ImagePanel(wx.Panel):
+    """This panel displays the images from the monitored camera."""
+ 
+    def __init__(self, parent):
+        """Constructor"""
+        wx.Panel.__init__(self, parent)
+        self.bitmap = wx.StaticBitmap(self, wx.ID_ANY, wx.EmptyBitmap(1, 1))
+        self.Bind(wx.EVT_LEFT_UP, self.on_click)
+ 
+    def on_click(self, event):
+        """Click event handler to check where was clicked and dispatch relevant event."""
+        # Here we add behaviour to check click position and
+        # take appropriate action.
+        pass
+
+    def update_image(self, image_stream):
+        """Update the bitmap with the specified stream."""
+        log.debug('Updating image from stream.')
+        self.bitmap.SetBitmap(wx.BitmapFromImage(wx.ImageFromStream(image_stream)))
+
+ 
---
+++ monitor.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+
+# General modules 
+import wx
+import os
+import io
+import sys
+import zmq
+import datetime
+import threading
+import socket
+import logging
+
+# Project modules
+from gui.imagepanel import ImagePanel
+import config 
+
+log = logging.getLogger(__name__)
+
+class MonitorFrame(wx.Frame):
+    """Frame holding image panel for monitor which runs maximised."""
+ 
+    def __init__(self):
+        """Constructor"""
+        wx.Frame.__init__(self, None, title="Camera Monitor")
+        
+        log.info('Loading network and monitor configuration files.')
+        module_dir = os.path.dirname(__file__)
+        self.network_config = config.load_from_file(os.path.join(module_dir, 'config/network.json'))
+        self.monitor_config = config.load_from_file(os.path.join(module_dir, 'config/monitor.json'))
+
+        log.info('Creating image panel.')
+        self.panel = ImagePanel(self)
+        self.ShowFullScreen(True)
+
+        log.info('Starting image retrieval background thread.')
+        self.thread = threading.Thread(target=self.image_retrieval_loop)
+        self.thread.start()
+
+
+    def image_retrieval_loop(self):
+        reload_delay = self.monitor_config['image_reload_delay']
+        camera_ip_addrs = self.monitor_config['camera_ip_addrs']
+
+        eventport = self.network_config['event_pub_port']
+        jpegport = self.network_config['jpeg_router_port']
+
+        context = zmq.Context()
+
+        eventsockets = []
+        jpegsockets = {}
+        jpegsocketstoip = {}
+
+        for cameraip in camera_ip_addrs:
+            log.info('Connecting to camera event feed: %s:%i.', cameraip, eventport)
+            eventsocket = context.socket(zmq.SUB)
+            eventsocket.connect('tcp://%s:%s' % (cameraip, eventport))
+            eventsocket.setsockopt(zmq.SUBSCRIBE, '')
+            eventsockets += [eventsocket]
+
+            log.info('Connecting to camera JPEG feed: %s:%i.', cameraip, jpegport)
+            jpegsocket = context.socket(zmq.REQ)
+            jpegsocket.connect('tcp://%s:%s' % (cameraip, jpegport))
+            jpegsockets[cameraip] = jpegsocket
+            jpegsocketstoip[jpegsocket] = cameraip
+
+        poller = zmq.Poller()
+        for jpegsocket in jpegsockets.itervalues():
+            poller.register(jpegsocket, zmq.POLLIN)
+
+        for eventsocket in eventsockets:
+            poller.register(eventsocket, zmq.POLLIN)
+
+        ongoing_reqs = {}
+        last_reqs = dict((ip, datetime.datetime.min) for ip in camera_ip_addrs)
+
+        while True:
+            socks = dict(poller.poll(100))
+            for sock, status in socks.iteritems():
+                if sock in jpegsocket.items() and status == zmq.POLLIN: 
+                    data = sock.recv()
+                    if data:
+                        ongoing_reqs[jpegsocketstoip[sock]].write(data)
+                        sock.send('NEXT')
+                    else:
+                        stream = ongoing_reqs[jpegsocketstoip[sock]]
+                        stream.seek(0)
+                        wx.CallAfter(self.panel.update_image, stream)
+                        del ongoing_reqs[jpegsocketstoip[sock]]
+                        sock.send('ENDACK')
+                        sock.recv()
+
+                elif sock in eventsockets and status == zmq.POLLIN: 
+                    host, data = sock.recv_multipart()
+            
+            for ip in camera_ip_addrs:
+                if ip not in ongoing_reqs and datetime.datetime.now() > last_reqs[ip] + datetime.timedelta(seconds=reload_delay):
+                    last_reqs[ip] = datetime.datetime.now()
+                    ongoing_reqs[ip] = io.BytesIO()
+                    jpegsockets[ip].send('NEW') 
+
+ 
+if __name__ == "__main__":
+    # Setup logging    
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    root.addHandler(ch)
+
+    app = wx.App(False)
+    frame = MonitorFrame()
+    app.MainLoop()
+
+
